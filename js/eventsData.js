b3fc61a430220a651a42191a805d601abe91df57
Njinu <99141352+NjinuParam@users.noreply.github.com>
2025-09-09 21:24:44 +0200

    feat(up)

--- js/eventsData.js
+++ js/eventsData.js
@@ -3,11 +3,11 @@ const eventsData = {
   "event1": {
     name: "Evangelism Drive",
     location: "Rosebank Mall (Starbucks)",
-    date: "2025-08-16",
+    date: "2025-09-20",
     description: "Join us for an evangelism drive at Rosebank Mall. We are sharing the gospel with the public and inviting them to learn more about Jesus and the good news he brought.",
     url: "./event.html?event=event1",
-    lumaUrl: "https://lu.ma/event/evt-UmMdc7pOwBCuYiS",
-    lumaEventId: "evt-UmMdc7pOwBCuYiS",
+    lumaUrl: "https://lu.ma/event/evt-mT9eZi8AWyp7iJB",
+    lumaEventId: "evt-mT9eZi8AWyp7iJB",
     time: "12:30 AM",
     image:"img/9.jpg",
     type:"event"
@@ -15,7 +15,7 @@ const eventsData = {
   "event2": {
     name: "Bible Donations",
     location: "General - Johannesburg",
-    date: "2025-08-24",
+    date: "2025-09-24",
     description: "We are collecting bibles and funds to purchase Bibles and distribute them to communities that lack access to the Word of God. Each NT bible we buy is R17.00",
     url: "./event.html?event=event2",
     lumaUrl: "https://lu.ma/event/evt-UmMdc7pOwBCuYiS",
@@ -25,15 +25,15 @@ const eventsData = {
     type:"donation"
   },
   "event3": {
-    name: "Food Drive",
+    name: "Clothes Drive",
     location: "WaterWorks Soweto",
-    date: "2025-08-30",
-    description: "Join us for a recurring food drive at WaterWorks Soweto. We are collecting non-perishable food items and bibles to distribute to those in need.",
+    date: "2025-09-13",
+    description: "Join us for a recurring clothes drive at WaterWorks Soweto. We are distributing gently used clothing items to those in need.",
     url: "./event.html?event=event3",
-    lumaUrl: "https://lu.ma/event/evt-UmMdc7pOwBCuYiS",
-    lumaEventId: "evt-UmMdc7pOwBCuYiS",
-    time: "9:00 AM",
+    lumaUrl: "https://lu.ma/event/evt-4woKzE4OOkIq0cT",
+    lumaEventId: "evt-4woKzE4OOkIq0cT",
+    time: "11:00 AM",
     image:"img/2.jpg",
-    type:"outreach"
+    type:"event"
   }
 };
